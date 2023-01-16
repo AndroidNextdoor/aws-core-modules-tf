414b241d68bb6fea13a17af9a634d7b1b27ecefd
AndroidNextdoor <nixdorfa@gmail.com>
2023-01-15 21:56:10 -0800

    Role Arn Additions on outputs

--- iam-resources/outputs.tf
+++ iam-resources/outputs.tf
@@ -1,19 +1,29 @@
-output "resource_admin_role_name" {
+output "resource_devops_admin_role_name" {
   value       = aws_iam_role.admin_access_role.name
   description = "The name of the role users are able to assume to attain admin privileges"
 }
 
-output "resource_admin_role_arn" {
+output "resource_devops_admin_role_arn" {
   value       = aws_iam_role.admin_access_role.arn
   description = "The ARN of the role users are able to assume to attain admin privileges"
 }
 
-output "resource_user_role_name" {
+output "resource_developer_role_name" {
   value       = aws_iam_role.developer_access_role.name
   description = "The name of the role users are able to assume to attain user privileges"
 }
 
-output "resource_user_role_arn" {
+output "resource_developer_role_arn" {
   value       = aws_iam_role.developer_access_role.arn
+  description = "The ARN of the role users are able to assume to attain user privileges"
+}
+
+output "resource_limited_role_name" {
+  value       = aws_iam_role.limited_access_role.name
+  description = "The name of the role users are able to assume to attain user privileges"
+}
+
+output "resource_limited_role_arn" {
+  value       = aws_iam_role.limited_access_role.arn
   description = "The ARN of the role users are able to assume to attain user privileges"
 }
